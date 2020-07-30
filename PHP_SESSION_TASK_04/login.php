69fdde65a810163112f1ef918586adf1e0f60040
SumnanAzadi <anadaplay@gmail.com>
2020-07-30 21:57:16 +0600

    login page some changes

--- PHP_SESSION_TASK_04/login.php
+++ PHP_SESSION_TASK_04/login.php
@@ -56,7 +56,7 @@ if(isset($_POST['submit'])){
     		<input name="remember" type="checkbox">Remember Me
     		<br/><br/>
             <input type="submit" value="Submit">        
-    		<a href="forgot_password.html">Forgot Password?</a>
+    		<a href="forgot_password.php">Forgot Password?</a>
         </form>
     </fieldset>
 </body>
