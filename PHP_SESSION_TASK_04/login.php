98dae6b0deadac77a71dcc3b923ba8a0a20273ad
SumnanAzadi <anadaplay@gmail.com>
2020-07-30 21:56:46 +0600

    login page done

--- PHP_SESSION_TASK_04/login.php
+++ PHP_SESSION_TASK_04/login.php
@@ -5,10 +5,11 @@ if(isset($_POST['submit'])){
     $password =md5($_POST['password']);
     if(empty($name) || empty($password) ){
         echo "null submission found!";
-    }else{
-        setcookie('name',$name, time()+3600);
-        setcookie('password',$password, time()+3600);
-        header('Location: edit_profile.php');
+    }else if(isset($_SESSION['userName']) && isset($_SESSION['password'])){
+        if($name == $_SESSION['userName'] && $password == $_SESSION['password']){
+                $_SESSION['status'] = "OK";
+                header('Location: edit_profile.php');
+            }
     }
 }else{
     header('location: login.php');
@@ -19,6 +20,23 @@ if(isset($_POST['submit'])){
     <title>Login</title>
 </head>
 <body>
+    <center>
+        <table width="100%" border="1">
+            <tr height="100px">
+                <td width="100px">
+                    <a href="index.html"><img src="./logo (1).png" width="150px" height="100px"></a>
+                <td align="right" colspan="2">
+                    <a href="login.php">Home  | </a>
+                    <a href="login.php">login  | </a>
+                    <a href="login.php">Registration     | </a>
+                </td>
+                </td>
+            </tr>
+            <tr width="70%" height="300px">
+                <td colspan="3">
+            </tr>
+        </table>
+		<center>
     <fieldset>
         <legend><b>LOGIN</b></legend>
         <form method="POST" action="<?php echo $_SERVER['PHP_SELF'];?>">
