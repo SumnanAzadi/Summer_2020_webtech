83c50749d51b4d26d5094fa98fdea3e4f5660638
SumnanAzadi <anadaplay@gmail.com>
2020-07-30 22:05:49 +0600

    minos typo changes

--- PHP_SESSION_TASK_04/login.php
+++ PHP_SESSION_TASK_04/login.php
@@ -24,7 +24,7 @@ if(isset($_POST['submit'])){
         <table width="100%" border="1">
             <tr height="100px">
                 <td width="100px">
-                    <a href="index.html"><img src="./logo (1).png" width="150px" height="100px"></a>
+                    <a href="index.html"><img src="./logo.png" width="150px" height="100px"></a>
                 <td align="right" colspan="2">
                     <a href="login.php">Home  | </a>
                     <a href="login.php">login  | </a>
